48081130aaee696fed07f783c40a3c5b2f9cb39b
Jak Daniels <jak@ateb.co.uk>
2015-07-24 22:12:48 +0100

    Major refactor of directories

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+mkdir lib/ 2>/dev/null
+mkdir etc/ 2>/dev/null
+
 echo "Using SWIG to create PHP module source..."
-swig -php -prefix A10Lime A10Lime.i
+cd src/
+swig -php -prefix A10Lime_ A10Lime.i
+mv -f A10Lime.php ../php-lib/
+
 echo "Compiling PHP module source..."
 gcc `php-config --includes` -fpic -c A10Lime_wrap.c
-gcc -shared A10Lime_wrap.o -lA10Lime-gpio -lA10Lime-i2c -lA10Lime-spi -o A10Lime.so
+gcc -shared A10Lime_wrap.o -lA10Lime-gpio -lA10Lime-i2c -lA10Lime-spi -o ../lib/A10Lime.so
 
 echo "Copying A10Lime.so to PHP extensions dir..."
-echo "extension=`php-config --extension-dir`/A10Lime.so" >A10Lime.ini
+echo "extension=`php-config --extension-dir`/A10Lime.so" >../etc/A10Lime.ini
 
-sudo cp -f A10Lime.so `php-config --extension-dir`/
-sudo cp -f A10Lime.ini /etc/php5/conf.d/90-A10Lime.ini
+sudo cp -f ../lib/A10Lime.so `php-config --extension-dir`/A10Lime.so
+sudo cp -f ../etc/A10Lime.ini /etc/php5/conf.d/90-A10Lime.ini
 sudo chown root:root `php-config --extension-dir`/A10Lime.so /etc/php5/conf.d/90-A10Lime.ini
 
 echo "There is a A10Lime.php include file that loads the module and provides a A10Lime class."
---
+++ src/A10Lime.i
@@ -0,0 +1,10 @@
+%module A10Lime
+%{
+#include "stdint.h"
+#include "../gpio/gpio_lib.h"
+#include "../i2c/i2c_lib.h"
+#include "../spi/spi_lib.h"
+%}
+%include "../gpio/gpio_lib.h"
+%include "../i2c/i2c_lib.h"
+%include "../spi/spi_lib.h"
